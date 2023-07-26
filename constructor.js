bec31aae82364d577297fa51739d1ea95235e16d
aidencuneo <aidencuneo@gmail.com>
2023-07-26 20:28:30 +1000

    p

---
+++ constructor.js
@@ -0,0 +1,20 @@
+// This file has polymorphism ;)
+
+class Something {
+    constructor(a, b) {
+        
+    }
+}
+
+var thing = new Something('bob', 'aiden');
+
+class Parent {
+    breathe = () => {}
+}
+
+// Inheritance
+class Child extends Parent {
+    // change slightly
+
+    tantrum = () => {}
+}
